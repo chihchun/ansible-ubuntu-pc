fb118351ec77720fe45996fb497e034c8d300d3f
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2020-08-20 17:20:33 +0800

    move the path for checkbox task

--- run-checkbox.sh
+++ run-checkbox.sh
@@ -3,6 +3,6 @@ set -x
 
 HOST=${1:-localhost}
 JOB=${2:-com.canonical.certification::smoke}
-TASKS=enablement-focal/tasks/checkbox-runner.yaml
+TASKS=roles/enablement-focal/tasks/checkbox-runner.yaml
 
 ansible-playbook -vv --extra-vars="hosts=$HOST" --extra-vars="task=${TASKS}" --extra-vars="checkbox_job=${JOB}" task-runner.yaml
