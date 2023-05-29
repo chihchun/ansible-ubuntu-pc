f5a595dac7a53d040e18a057cd906e69a06fec88
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2023-05-29 17:27:02 +0800

    script: ask for sudo password when deploying.

--- run-task.sh
+++ run-task.sh
@@ -4,4 +4,4 @@ set -x
 HOST=${1:-localhost}
 TASKS=${2:-enablement-focal/tasks/hello.yaml}
 
-ansible-playbook -vv --extra-vars="hosts=$HOST" --extra-vars="task=${TASKS}" task-runner.yaml
+ansible-playbook -vv --ask-become-pass --extra-vars="hosts=$HOST" --extra-vars="task=${TASKS}" task-runner.yaml
